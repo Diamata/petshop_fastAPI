68aac4baef53f8a56b4b81d9718846a45afd314e
Diana Malyutina <diamalyutina@gmail.com>
2024-05-27 12:30:24 +0300

    delete characteristics router

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ from src.products.router import router as router_products
 from src.brands.router import router as router_brands
 from src.images.router import router as router_images
 from src.categories.router import router as router_categories
-from src.characteristics.router import router as router_characteristics
 
 app = FastAPI()
 
@@ -13,7 +12,6 @@ app.include_router(router_products)
 app.include_router(router_brands)
 app.include_router(router_images)
 app.include_router(router_categories)
-app.include_router(router_characteristics)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000)
