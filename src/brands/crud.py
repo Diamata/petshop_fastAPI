90ed37b09e5d2871435924462367f7016dacb1c7
Diana Malyutina <diamalyutina@gmail.com>
2024-07-30 13:40:45 +0300

    upd: brands crud refactored

--- src/brands/crud.py
+++ src/brands/crud.py
@@ -1,36 +1,12 @@
-from sqlalchemy import select, func, update
+from sqlalchemy import select
 
 from src.brands.models import Brand
-from src.brands.schemas import BrandSchema
 from src.repository.crud import BaseRepo
 from src.repository.db_connection import async_session_maker
 
 
 class BrandsRepo(BaseRepo):
     model = Brand
-
-    @classmethod
-    async def find_all_by_partial_name(cls, name: str):
-        async with async_session_maker() as session:
-            stmt = (
-                select(Brand)
-                .where(func.lower(Brand.name).contains(func.lower(name)))
-            )
-            response = await session.execute(stmt)
-            result = response.scalars().all()
-            return result
-
-    @classmethod
-    async def find_one_or_none_by_name(cls, name: str):
-        lowered_name: str = name.lower()
-        async with async_session_maker() as session:
-            stmt = (
-                select(Brand)
-                .where(func.lower(Brand.name) == lowered_name)
-            )
-            response = await session.execute(stmt)
-            result = response.scalar_one_or_none()
-            return result
 
     @classmethod
     async def create_new_brand(cls, name: str, description: str):
